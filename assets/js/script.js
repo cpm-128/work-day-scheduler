41ad1392e1274a9be30ee74b58923ec541cfdc0f
cpm-128 <cpm-128@users.noreply.github.com>
2022-05-19 13:58:00 -0400

    display time blocks for standard business hours

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,9 @@
+// elements on page
+containerEl = document.querySelector(".container");
+
+
+// display current day
 var currentDay = moment().format('dddd') + ", " + moment().format('ll');
     $("#currentDay").append(currentDay);
 
-var dayStart = "9"; // use 12 time, must be AM
-    var  formattedDayStart = moment(dayStart, "h").format('h:mm A');
-    console.log(formattedDayStart);
+
