f06b9477513b42409979f98e0b6b17a0416377ea
cpm-128 <cpm-128@users.noreply.github.com>
2022-05-19 11:46:13 -0400

    format time for day start in console

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,6 @@
 var currentDay = moment().format('dddd') + ", " + moment().format('ll');
     $("#currentDay").append(currentDay);
+
+var dayStart = "9"; // use 12 time, must be AM
+    var  formattedDayStart = moment(dayStart, "h").format('h:mm A');
+    console.log(formattedDayStart);
